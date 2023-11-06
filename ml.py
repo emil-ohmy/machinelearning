524e157732a4230a984e2f00c6f5d9cb2cb5ff47
Emil Johansson <emil@ohmy.co>
2023-11-06 12:42:04 +0100

    - Added requirements and gitignore

---
+++ ml.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+# save filepath to variable for easier access
+melbourne_file_path = 'melb_data.csv'
+# read the data and store data in DataFrame titled melbourne_data
+melbourne_data = pd.read_csv(melbourne_file_path)
+# print a summary of the data in Melbourne data
+#test = melbourne_data.describe()
+
+melbourne_data = pd.read_csv(melbourne_file_path)
+
+melbourne_data = melbourne_data.dropna(axis=0)
+y = melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = melbourne_data[melbourne_features]
+test = X.describe()
+test_n = X.head()
+
+melbourne_model = DecisionTreeRegressor(random_state=1)
+melbourne_model.fit(X, y)
+
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
