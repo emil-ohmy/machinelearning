96116d45b3d62f3e224e6eb3f4282ebd460d93f0
Emil Johansson <emil@ohmy.co>
2023-11-15 11:09:06 +0100

    - MOved stuff around

---
+++ explainability/ml1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import eli5
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from eli5.sklearn import PermutationImportance
+
+data = pd.read_csv('FIFA 2018 Statistics.csv')
+y = (data['Man of the Match'] == "Yes")  # Convert from string "Yes"/"No" to binary
+feature_names = [i for i in data.columns if data[i].dtype in [np.int64]]
+print(feature_names)
+X = data[feature_names]
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+my_model = RandomForestClassifier(n_estimators=100,
+                                  random_state=0).fit(train_X, train_y)
+
+
+perm = PermutationImportance(my_model, random_state=1).fit(val_X, val_y)
+eli5.show_weights(perm, feature_names = val_X.columns.tolist())
